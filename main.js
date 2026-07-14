dca53f0ecf36b7302529f8eb63832a10654c1ba8
Renjero <152833882+Renjero@users.noreply.github.com>
2026-07-15 01:56:01 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,79 @@
+let grid = document.getElementById('grid')
+let btn1 = document.getElementById('btn1')
+let btn2 = document.getElementById('btn2')
+let btn3 = document.getElementById('btn3')
+
+// let square = document.createElement('div')
+
+
+let count = 0
+const insertBox =()=>{
+    if(grid.children.length < 8){
+      let square = document.createElement('div')
+    square.className = "bg-yellow-600"
+    square.innerText = count
+    grid.appendChild(square)
+    console.log(grid.children.length)
+    count = count+1
+    }
+    else{
+  showPopup("NO more space left for box"); // Function call 
+    }
+}
+btn1.addEventListener("click", insertBox)
+  
+
+const remove =() => {
+    // assiment one by ekills removee from 1st
+    grid.removeChild(grid.firstElementChild)
+    count --
+}
+btn2.addEventListener("click", remove)
+
+// let currentIndex = 0;
+// const changeColor = () => {
+//     grid.children[currentIndex].classList.replace("bg-yellow-600", "bg-blue-600");
+//     currentIndex++;
+//     console.log(currentIndex);
+// };
+// btn3.addEventListener("click", changeColor);
+let currentIndex = 0;
+const changeColor = (message) => {
+    if (currentIndex < grid.children.length) {
+        grid.children[currentIndex].classList.replace(
+            "bg-yellow-600",
+            "bg-blue-600"
+        );
+
+        currentIndex++;
+    } else {
+        showPopup("All boxes are blue!");
+    }
+};
+btn3.addEventListener("click", changeColor);
+
+
+
+
+
+// code popuup by ai 
+function showPopup(message) {
+    const popup = document.createElement("div");
+
+    popup.className =
+        "fixed inset-0 flex items-center justify-center bg-black/40";
+
+    popup.innerHTML = `
+        <div class="bg-white p-6 rounded-xl shadow-xl text-center w-72">
+            <h2 class="text-xl font-bold text-red-600 mb-2"> Sorry </h2>
+            <p class="mb-4">${message}</p>
+            <button class="bg-green-500 hover:bg-red-600 text-white px-5 py-2 rounded-lg">
+                OK
+            </button>
+        </div>
+    `;
+
+    document.body.appendChild(popup);
+
+    popup.querySelector("button").onclick = () => popup.remove();
+}
